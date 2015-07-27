c2a1129494eb15d49389d5acf55a1b0f591b45e0
baiyang <baiyang@in66.com>
2015-07-27 20:49:37 +0800

    提交文件

---
+++ VEStatusModel.h
@@ -0,0 +1,13 @@
+//
+//  VEStatusModel.h
+//  
+//
+//  Created by baiyang on 15/7/27.
+//
+//
+
+#import "MTLModel.h"
+
+@interface VEStatusModel : MTLModel
+
+@end
---
+++ VEStatusModel.m
@@ -0,0 +1,13 @@
+//
+//  VEStatusModel.m
+//  
+//
+//  Created by baiyang on 15/7/27.
+//
+//
+
+#import "VEStatusModel.h"
+
+@implementation VEStatusModel
+
+@end
