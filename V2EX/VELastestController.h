8f13bb9c6374f26a0ad6896b65a034e9b44def60
翁佳 <656028411@qq.com>
2015-07-28 01:28:00 -0700

    最新模块文件提交

---
+++ V2EX/VELastestController.h
@@ -0,0 +1,13 @@
+//
+//  VELastestController.h
+//  V2EX
+//
+//  Created by 翁佳 on 15-7-28.
+//  Copyright (c) 2015年 owl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VELastestController : NSObject
+
+@end
---
+++ V2EX/VELastestController.m
@@ -0,0 +1,13 @@
+//
+//  VELastestController.m
+//  V2EX
+//
+//  Created by 翁佳 on 15-7-28.
+//  Copyright (c) 2015年 owl. All rights reserved.
+//
+
+#import "VELastestController.h"
+
+@implementation VELastestController
+
+@end
